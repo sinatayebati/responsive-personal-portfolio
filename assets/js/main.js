00237faa8966743ac77b27c8517e534fc1d28710
sinatayebati <sinatayebati@gmail.com>
2023-05-28 13:03:23 -0500

    updates commited

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,23 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'Skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEeach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
