aca25fa20b91899538f681e1e2041e8391672d38
sinatayebati <sinatayebati@gmail.com>
2023-06-01 23:37:50 -0500

    updates commited

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,7 +93,18 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
 
 /*==================== TESTIMONIAL ====================*/
 
